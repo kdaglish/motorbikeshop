079f8c29317d152981ebdc3e5dc4beae10cf5637
Kevin Daglish <kevindaglish@kevins-mbp.hursley.uk.ibm.com>
2016-12-06 11:26:34 +0000

    app routing and working

--- app/js/app.js
+++ app/js/app.js
@@ -1,71 +1,24 @@
-var app = angular.module('bikeStore', []);
+angular.module('bikeStore', [
+  'ngRoute',
+  'helmetProducts',
+  'bikeProducts'
+])
 
-app.controller('bikeController', function() {
-  this.tabIndex = 1;
+.config(function($routeProvider){
+  $routeProvider.
+  when('/bikes', {
+    template: '<bikes-list></bikes-list>'
+  }).
+  when('/helmets', {
+    template: '<helmets-list></helmets-list>'
+  }).
+  otherwise('/bikes');
+})
 
-  this.tabChange = function(tabIndex){
-    this.tabIndex = tabIndex;
+.controller('primaryTabController', function(){
+  this.primaryTabIndex = 1;
+
+  this.primaryTabChange = function(TabIndex){
+    this.primaryTabIndex = TabIndex;
   }
-
-  this.bikes =
-  [
-    {
-      name: 'Honda CBR600rr',
-      description: "The CBR600RR",
-      price: 9000.00,
-      specs: {
-        engineType: "599 cc liquid-cooled inline four cylinder",
-        power: "105.5 hp",
-        brakes: "Dual radial-mounted four-piston calipers with 310.0 mm (12.20 in) discs"
-      },
-      images: [
-        "bike_images/cbr600rr-01.jpg",
-        "bike_images/cbr600rr-02.jpg"
-      ],
-      reviews: [
-        {
-          stars: 5,
-          body: "I love this gem!",
-          author: "joe@example.org",
-          createdOn: 1397490980837
-        },
-        {
-           stars: 1,
-           body: "This gem sucks.",
-           author: "tim@example.org",
-           createdOn: 1397490980837
-        }
-      ]
-    },
-    {
-      name: 'Yamaha YZF-R6',
-      description: "yahamahahaha",
-      price: 9999.99,
-      specs: {
-        engineType: "599 cc liquid-cooled inline four-cylinder",
-        power: "108.5 hp",
-        brakes: "Dual radial-mounted four-piston calipers with 310.0 mm (12.20 in) discs"
-      },
-      images: [
-        "bike_images/r6-01.jpg",
-        "bike_images/r6-02.jpg"
-      ],
-      reviews: [
-        {
-          stars: 3,
-          body: "I think this gem was just OK, could honestly use more shine, IMO.",
-          author: "JimmyDean@example.org",
-          createdOn: 1397490980837
-        },
-        {
-          stars: 4,
-          body: "Any gem with 12 faces is for me!",
-          author: "gemsRock@example.org",
-          createdOn: 1397490980837
-        }
-      ]
-    }
-  ];
-
-
 });
---
+++ app/js/bike.js
@@ -0,0 +1,71 @@
+angular.module('bikeProducts', [])
+
+.directive("bikesList", function(){
+  return{
+    restrict: 'E',
+    templateUrl: "bikes.html"
+  };
+})
+
+.controller('bikeController', function() {
+
+  this.bikes =
+  [
+    {
+      name: 'Honda CBR600rr',
+      description: "The CBR600RR",
+      price: 9000.00,
+      specs: {
+        engineType: "599 cc liquid-cooled inline four cylinder",
+        power: "105.5 hp",
+        brakes: "Dual radial-mounted four-piston calipers with 310.0 mm (12.20 in) discs"
+      },
+      images: [
+        "bike_images/cbr600rr-01.jpg",
+        "bike_images/cbr600rr-02.jpg"
+      ],
+      reviews: [
+        {
+          stars: 5,
+          body: "I love this gem!",
+          author: "joe@example.org",
+          createdOn: 1397490980837
+        },
+        {
+           stars: 1,
+           body: "This gem sucks.",
+           author: "tim@example.org",
+           createdOn: 1397490980837
+        }
+      ]
+    },
+    {
+      name: 'Yamaha YZF-R6',
+      description: "yahamahahaha",
+      price: 9999.99,
+      specs: {
+        engineType: "599 cc liquid-cooled inline four-cylinder",
+        power: "108.5 hp",
+        brakes: "Dual radial-mounted four-piston calipers with 310.0 mm (12.20 in) discs"
+      },
+      images: [
+        "bike_images/r6-01.jpg",
+        "bike_images/r6-02.jpg"
+      ],
+      reviews: [
+        {
+          stars: 3,
+          body: "I think this gem was just OK, could honestly use more shine, IMO.",
+          author: "JimmyDean@example.org",
+          createdOn: 1397490980837
+        },
+        {
+          stars: 4,
+          body: "Any gem with 12 faces is for me!",
+          author: "gemsRock@example.org",
+          createdOn: 1397490980837
+        }
+      ]
+    }
+  ];
+});
